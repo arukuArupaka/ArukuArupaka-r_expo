f6cff12be088a7a862a16bed56fde083740ca8ca
soshiMac <nakatanishoushi@gmail.com>
2024-01-31 23:24:18 +0900

    backendapp追加

--- backend/arukuarupaka/settings.py
+++ backend/arukuarupaka/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'home_view',
     'textbook_market',
+    'time_table',
 ]
 
 MIDDLEWARE = [
---
+++ backend/time_table/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+# Create your views here.
+@api_view(['GET'])
+def helloworldfunc(request):
+  person = {'name':'テスト', 'コードを書く場所':'time_tableフォルダの中は自由に変えてもらって大丈夫です。'}
+  return Response(person)
