16a247fe2deb775a3bc5450c54b51a900ad6ee34
show-creator <git>
2024-03-26 02:06:15 +0900

    いったんコミット

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,8 @@ set -o errexit
 pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic
+python3 manage.py createsuperuser
 python3 manage.py migrate
-python manage.py flush --noinput
+python3 manage.py flush --noinput
 python3 manage.py loaddata data.json
