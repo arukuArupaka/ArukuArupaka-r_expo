55d628724f318bcf612d6aba241232d6ca66258d
show-creator <git>
2024-03-26 16:06:53 +0900

    少し修正

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,7 @@ pip3 install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py collectstatic
 python3 manage.py createsuperuser
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py flush --noinput
 python3 manage.py loaddata data.json
