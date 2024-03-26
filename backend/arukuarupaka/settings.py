b891735fc89847fafc2d791d1d9f5239cc4015df
show-creator <git>
2024-03-26 17:05:30 +0900

    修正

--- backend/arukuarupaka/settings.py
+++ backend/arukuarupaka/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'arukuarupaka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': 'render_test_database',
         'USER': 'kamoku_user',
