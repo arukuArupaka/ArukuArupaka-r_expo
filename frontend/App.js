79c78eb21db88762831ced91bb588307077cb360
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-03-27 01:29:16 +0900

    fixup! Merge pull request #44 from soshi1234/refineWeatherPage

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
-import HomeView from "./src/View/HomeView";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import TimeTable from "./src/View/TimeTableView";
-import BikeView from "./src/View/BikeView";
-import WeatherView from "./src/View/weather";
-import { Textbook } from "./src/View/Textbook/TextbookView";
-import MapView from "./src/View/Map/MapMain";
-import TimeTableRoot from "./src/View/TimeTableViewNavigateRoot";
-import ASetting from "./src/View/ASetting";
-import { TouchableOpacity, Image, View } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import TimeTableSetting from "./src/View/TimeTableSetting";
-import { TimeTableProvider } from "./src/component/TimeTable/TimeTableContext";
-import LogoTitle from "./src/component/Map/headerForMap";
-import ALoginView from "./src/View/ALoginView";
-import MapLoot from "./src/component/Map/MapLoot";
-import TimeTableClass from "./src/View/TimeTableClass";
-import KomaView from "./src/View/KomaView";
-import WebSite from "./src/View/WebSite";
-import { Provider } from "react-redux";
-import AR_Store from "./src/redux/store";
-import ASettingToPage from "./src/View/ASettingToPage";
-=======
 import HomeView from './src/View/HomeView'
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TimeTable from './src/View/TimeTableView'
 import BikeView from './src/View/BikeView'
-import WeatherView from './src/View/Weather'
+import WeatherView from './src/View/weather'
 import {Textbook} from './src/View/Textbook/TextbookView';
 import MapView from './src/View/Map/MapMain'
 import TimeTableRoot from './src/View/TimeTableViewNavigateRoot'
@@ -48,7 +23,6 @@ import AR_Store from './src/redux/store';
 import ASettingToPage from './src/View/ASettingToPage';
 import * as Notifications from 'expo-notifications';
 import React from 'react';
->>>>>>> bf9c927304b711193ccfad872a5dbc26936ea0db
 
 const Stack = createNativeStackNavigator();
 
@@ -84,9 +58,8 @@ function App() {
 
   return (
     <Provider store={AR_Store}>
-<<<<<<< HEAD
       <TimeTableProvider>
-        <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen
               name="Home"
@@ -125,31 +98,6 @@ function App() {
                 headerShown: false,
               }}
             />
-=======
-    <TimeTableProvider>
-      <NavigationContainer ref={navigationRef}>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name="Home" component={HomeView} options={{ headerShown: false }}/>
-          <Stack.Screen name="TimeTable" component={TimeTable}
-            options={({ navigation }) => ({
-              title: '',
-              headerRight: () => (
-                <TouchableOpacity onPress={() => navigation.navigate('TimeTableSetting')}>
-                  <AntDesign name="setting" size={24} color="black" />
-                </TouchableOpacity>
-              ),
-            })} />
-          <Stack.Screen name="Bike" component={BikeView}/>
-          <Stack.Screen name="Weather" component={WeatherView}/>
-          <Stack.Screen name="Map" component={MapLoot}
-            options={{headerTitle: (props) => <LogoTitle {...props} />}}/>
-          <Stack.Screen name="textbook" component={Textbook} 
-           options={{
-            headerShown:false
-           }}
-          />
->>>>>>> bf9c927304b711193ccfad872a5dbc26936ea0db
-
             <Stack.Screen
               name="settings"
               component={ASetting}
--- frontend/src/View/HomeView.tsx
+++ frontend/src/View/HomeView.tsx
@@ -205,7 +205,7 @@ const HomeView = (props) => {
             appName="天気予報"
             color="#EB3637"
             test={props}
-            jumpPage="Weather"
+            jumpPage="weather"
             iconName="weather-partly-cloudy"
           />
         </View>
