44226aa702179bc691a88275d6b71e6c5520812f
soshiBuildMac <nakatanisousi@gmail.com>
2024-04-28 17:22:51 +0900

    fixup! fixup! データベース変更

--- backend/home_view/models.py
+++ backend/home_view/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Photo(models.Model):
     title = models.CharField(max_length=255, default="Untitled")
     image = models.ImageField(upload_to='photos/')
-    description = models.TextField()
+    description = models.TextField(max_length=255, default="")
     carousel_url = models.URLField(default="")
     
 
--- backend/home_view/serializer.py
+++ backend/home_view/serializer.py
@@ -8,7 +8,7 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
-        fields = ['id', 'title', 'description', 'image', 'image_url', 'carousel_url']  # carousel_url を fields に追加
+        fields = ['title', 'description', 'image', 'image_url', 'carousel_url']  # carousel_url を fields に追加
 
     def get_image_url(self, obj):
         request = self.context.get('request')
