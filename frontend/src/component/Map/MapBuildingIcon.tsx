2f3d7799f105623215f776752a3c8908cb027933
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2024-05-25 04:41:46 +0900

    discordサーバーを廃止

--- frontend/src/component/Map/MapBuildingIcon.tsx
+++ frontend/src/component/Map/MapBuildingIcon.tsx
@@ -14,7 +14,7 @@ const MapBuildingIcon = (props) => {
         title={props.buildingData.buildingName}
         description={props.buildingData.buildingType+"\n"+props.buildingData.buildingDetail}
     >
-    <Image style={{top:9,right:8,width:40,height:40,borderRadius:25,position:'absolute'}} source={{uri:props.buildingData.buildingImage?props.buildingData.buildingImage:"https://media.discordapp.net/attachments/1210241561095573504/1219219360976080987/24660942.jpg?ex=660a8183&is=65f80c83&hm=b3e22c638cb150a3ee37a0a7f0c228ce0c7b79130843af5cdb3597c8ee912b66&=&format=webp&width=1350&height=1018"}}/>
+    <Image style={{top:9,right:8,width:40,height:40,borderRadius:25,position:'absolute'}} source={props.buildingData.buildingImage?{uri:props.buildingData.buildingImage}:require("../../image/Logo.png")}/>
     <Ionicons name="chatbubble-outline" style={{transform:[{rotate: '-45deg'}]}} size={55} color="black" />
     {/* <Callout>
       <View>
