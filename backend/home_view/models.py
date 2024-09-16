0499514c3f5b4a099e2aaa5c3c8b862d9bed62a9
show-creator <git>
2024-09-16 20:47:00 +0900

    しゅうせい2

--- backend/home_view/models.py
+++ backend/home_view/models.py
@@ -4,7 +4,7 @@ class Photo(models.Model):
     title = models.CharField(max_length=255, default="Untitled")
     image = models.ImageField(upload_to='photos/')
     description = models.TextField(max_length=255, default="")
-    carousel_url = models.URLField(default="")
+    carousel_url = models.URLField(default="", null=True, blank=True)
     
 
     def __str__(self):
