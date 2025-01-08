1a7ab60d121643cac62811878b3fce9c9a9a73bf
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2025-01-09 02:22:29 +0900

    リリース

--- frontend/src/View/Textbook/main/TextbookTalkView.tsx
+++ frontend/src/View/Textbook/main/TextbookTalkView.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 // import {View, Text,TouchableOpacity} from 'react-native';
 // import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { MaterialIcons,MaterialCommunityIcons ,Ionicons,Entypo } from '@expo/vector-icons';
-import { TextbookHome } from './TextbookHome';
-import { TextbookTalk } from './Talk/TextbookTalk';
-import { TextbookCamera } from './TextbookCamera';
-import { TextbookMyPage } from './TextbookMyPage';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  MaterialIcons,
+  MaterialCommunityIcons,
+  Ionicons,
+  Entypo,
+} from "@expo/vector-icons";
+import { TextbookHome } from "./TextbookHome";
+import { TextbookTalk } from "./Talk/TextbookTalk";
+import { TextbookCamera } from "./TextbookCamera";
+import { TextbookMyPage } from "./TextbookMyPage";
 import { Chatroom } from "./Talk/Chatroom";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { TalkProvider } from '../../../component/Textbook/Chat/TalkContext';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TalkProvider } from "../../../component/Textbook/Chat/TalkContext";
 
 // const Stack = createStackNavigator();
 const Stack = createStackNavigator();
@@ -20,20 +25,23 @@ export const TextbookTalkView = () => {
     <TalkProvider>
       <Stack.Navigator
         screenOptions={{
-          headerShown:false,
-      }}>
-          <Stack.Screen name='トークルーム' component={TextbookTalk}
-            options={{
-              headerShown:false,
-            }}
-          >
-          </Stack.Screen>
-          <Stack.Screen name='チャットルーム' component={Chatroom}
-            options={{
-              headerShown:false,
-            }}
-          >
-          </Stack.Screen>
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="トークルーム"
+          component={TextbookTalk}
+          options={{
+            headerShown: false,
+          }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="チャットルーム"
+          component={Chatroom}
+          options={{
+            headerShown: false,
+          }}
+        ></Stack.Screen>
       </Stack.Navigator>
     </TalkProvider>
   );
