5fee21ebb7668e8d05cbfb63556a923b44b851a0
soshi1234 <148639191+soshi1234@users.noreply.github.com>
2025-04-06 18:43:50 +0900

    広告対応

--- frontend/src/component/Home/HomeAdmobFooter.tsx
+++ frontend/src/component/Home/HomeAdmobFooter.tsx
@@ -1,18 +1,27 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+} from "react-native-google-mobile-ads";
 
 const HomeAdmobFooter = () => {
   return (
-    <View
-      style={{
-        backgroundColor: "gray",
-        height: 50,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* Admob Footer */}
-      <Text>広告！！！！</Text>
+    <View style={{ alignItems: "center" }}>
+      <BannerAd
+        unitId={"ca-app-pub-7782777506427620/3276047368"} // ★ 本番はここを本物のAd Unit IDに置き換える
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+        onAdLoaded={() => {
+          console.log("Ad loaded");
+        }}
+        onAdFailedToLoad={(error) => {
+          console.error("Ad failed to load: ", error);
+        }}
+      />
     </View>
   );
 };
