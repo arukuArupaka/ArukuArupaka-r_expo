1825faf6495f0806ab811ed1a889493ccb7af650
hideto <hideto_shimoie.23@icloud.com>
2025-05-21 17:53:35 +0900

    react-native/metro-configのインストール

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,28 @@
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  defaultConfig.resolver.sourceExts.push('cjs');
-  defaultConfig.resolver.unstable_enablePackageExports = false;
-  return defaultConfig;
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: { sourceExts, assetExts },
+} = defaultConfig;
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    // SVG を使わないならこの行は削除してOK
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    // SVG を画像として読み込めるようにする設定（不要なら削除OK）
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg', 'cjs'],
+    unstable_enablePackageExports: false,
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
