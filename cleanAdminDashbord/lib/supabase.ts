69f43b7ecb426a2d59c9588bc1bd5158ffebcabb
クルトン <beckeymiku@gmail.com>
2025-12-01 19:08:24 +0900

    環境変数の直書き修正

--- cleanAdminDashbord/lib/supabase.ts
+++ cleanAdminDashbord/lib/supabase.ts
@@ -1,13 +1,13 @@
 // lib/supabase.ts
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://aqcfoappnzfeqerovymn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxY2ZvYXBwbnpmZXFlcm92eW1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NDY3MjIsImV4cCI6MjA2NzAyMjcyMn0.vvVIrE5zttk5oM2XLHQZSTLbWZogpDh83aFTsHD5NEM'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true, // ブラウザにセッション保存
     autoRefreshToken: true, // トークン自動更新
-    storageKey: 'cleanAdminDashbord-auth', // 衝突防止のストレージキー
+    storageKey: "cleanAdminDashbord-auth", // 衝突防止のストレージキー
   },
-})
+});
